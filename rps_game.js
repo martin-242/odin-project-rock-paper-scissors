63b32f00c934822193c00ad129ee4bd3e9fa8482
martin-242 <martin242@gmail.com>
2023-10-26 19:00:25 +0200

    Replace while loop with for loop and adds comments

--- rps_game.js
+++ rps_game.js
@@ -18,45 +18,110 @@ function getComputerChoice () {
     }
 }
 
+function getPlayerChoice() {
+    let playerSelection = prompt("Please type Rock, Paper or Scissors:");
+    playerSelection = playerSelection.toLowerCase();
+    return playerSelection;
+}
+
 // playRound function:
 //      Takes the playerSelection and the computerSelection variables and compares them to determine who wins
 //      If player wins it returns 1 point and none if the computer wins.
 
-function playRound(playerSelection, computerSelection) {
-    playerSelection = playerSelection.toLowerCase()
-    console.log(`Player selected: "${playerSelection}"\nComputer selected: "${computerSelection}"\n`)
-    if ( playerSelection == computerSelection ) {
-        console.log("It's a tie!")  
-        return 0;
-    }
-    else if ( playerSelection == "rock" && computerSelection == "scissors" ) {
-        console.log("You won this round!")
-        return 1; 
-    }
-    else if ( playerSelection == "paper" && computerSelection == "rock" ) {
-        console.log("You won this round!") 
-        return 1; 
-    }
-    else if ( playerSelection == "scissors" && computerSelection == "paper" ) {
-        console.log("You won this round!")
-        return 1; 
-    }
-    else {
-        console.log("The computer wins this round") 
-        return 0;
+function playRound(playerSelection, computerSelection) {   
+        console.log(`Player selected: "${playerSelection}"\nComputer selected: "${computerSelection}"\n`);
+        if ( playerSelection == computerSelection ) {
+            console.log(`It's a tie!`);  
+            console.log(`Scores are: You "${playerScore}" - Computer "${computerScore}"`);
+            roundNumber = roundNumber + 1;
+        }
+        else if ( playerSelection == "rock" && computerSelection == "scissors" ) {
+            playerScore = playerScore + 1;
+            console.log(`You won this round!`);
+            console.log(`Scores are: You "${playerScore}" - Computer "${computerScore}"`);
+            roundNumber = roundNumber + 1;
+            return playerScore;
+        }
+        else if ( playerSelection == "paper" && computerSelection == "rock" ) {
+            playerScore = playerScore + 1;
+            console.log(`You won this round!`);
+            console.log(`Scores are: You "${playerScore}" - Computer "${computerScore}"`);
+            roundNumber = roundNumber + 1;
+            return playerScore;
+        }
+        else if ( playerSelection == "scissors" && computerSelection == "paper" ) {
+            playerScore = playerScore + 1;
+            console.log(`You won this round!`);
+            console.log(`Scores are: You "${playerScore}" - Computer "${computerScore}"`);
+            roundNumber = roundNumber + 1;
+            return playerScore;
+        }
+        else {
+            computerScore = computerScore + 1;
+            console.log(`Computer won this round!`); 
+            console.log(`Scores are: You "${playerScore}" - Computer "${computerScore}"`);
+            roundNumber = roundNumber + 1;
+            return computerScore;
+        }
+}
+
+// Game function:
+// Runs a for loop 5 times and on each round gets the user and computer choices and executes the playRound function. 
+// If the player or the computer gets 3 points it wins, if not, whoever has more points in the 5th round wins.
+// If the player and computer have the same points on round 5 then it's a tie.
+
+function game() {    
+    console.log("Welcome to Rock/Paper/Scissors!");
+    for ( let i = 0; i <= 4; i++ ) {
+        if ( roundNumber == 1 ) {
+            console.log("Let's start! Round 1!")
+            }
+        else {
+            console.log(`Round ${roundNumber}`);  
+        }
+        console.log("Please make your selection!");
+        computerSelection = getComputerChoice();
+        playerSelection = getPlayerChoice();
+        console.log(playRound(playerSelection, computerSelection));
+        if ( playerScore == 3 ) {
+            console.log("You have won the game!");
+            return keepGoing = false;
+        }
+        else if ( computerScore == 3 ) {
+            console.log("The computer won the game!");
+            return keepGoing = false;
+        }
+        else if ( i == 4) {
+            console.log(`Game over! Final scores are:`);
+            console.log(`Player: ${playerScore} - Computer: ${computerScore}`);
+            if ( playerScore == computerScore ) {
+                console.log(`The game is a tie!`);
+                return keepGoing = false;
+            }
+            else if ( playerScore > computerScore ) {
+                console.log(`You won the game!`);
+                return keepGoing = false;
+            }
+            else {
+                console.log(`The computer won the game!`);
+                return keepGoing = false;
+            }
+        }
+        else {
+            console.log("Lets keep playing! Next round!");
+        }
     }
 }
 
-// Game starts running below:
+// Initializes game variables
 
-// playerSelection variable is hardcoded for now, but later it will be inputted by the user
+let playerScore = 0;
+let computerScore = 0;
+let roundNumber = 1;
+let keepGoing = true;
+let computerSelection = 0;
+let playerSelection = 0;
 
-let playerSelection = "Paper";
+// Game function starts the game
 
-// The computerSelection is assigned running the getComputerChoice function
-
-const computerSelection = getComputerChoice();
-
-// The round is played taking the playerSelection and computerSelection into the playRound function. The result is printed on the console.
-
-console.log(playRound(playerSelection, computerSelection));
+game();
