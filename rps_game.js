6bb30d5fd6eef0e584397a53796712c27b9de987
martin-242 <martin242@gmail.com>
2023-10-26 14:59:48 +0200

    Finishes the exercise and adds comments

--- rps_game.js
+++ rps_game.js
@@ -1,62 +1,62 @@
-/* 
-
-getComputerChoice function
-    create a computerSelection variable
-    picks a random number from 1 to 3 and store it in computerChoice
-        if 1 choose Rock
-        if 2 choose Paper
-        else choose Scissors
-
-*/
+// getComputerChoice function:
+//      Selects a random number from 1 to 3 and stores it into a randomNumber variable
+//      Assigns to a variable computerSelection rock, paper or scissors depending on the number generated and returns it 
 
 function getComputerChoice () {
     let randomNumber = Math.floor(Math.random() * 3) + 1;
-    if ( randomNumber == 1 )
-        computerSelection = "rock" 
-    else if ( randomNumber == 2 )
-        computerSelection = "paper"
-    else ( computerSelection = "scissors" );
-
-    console.log(computerSelection);
+    if ( randomNumber == 1 ) {
+        let computerSelection = "rock" 
+        return computerSelection;
+    }
+    else if ( randomNumber == 2 ) {
+        let computerSelection = "paper"
+        return computerSelection;
+    }
+    else {
+        let computerSelection = "scissors" 
+        return computerSelection;
+    }
 }
 
-getComputerChoice();
+// playRound function:
+//      Takes the playerSelection and the computerSelection variables and compares them to determine who wins
+//      If player wins it returns 1 point and none if the computer wins.
 
-/*
+function playRound(playerSelection, computerSelection) {
+    playerSelection = playerSelection.toLowerCase()
+    console.log(`Player selected: "${playerSelection}"\nComputer selected: "${computerSelection}"\n`)
+    if ( playerSelection == computerSelection ) {
+        console.log("It's a tie!")  
+        return 0;
+    }
+    else if ( playerSelection == "rock" && computerSelection == "scissors" ) {
+        console.log("You won this round!")
+        return 1; 
+    }
+    else if ( playerSelection == "paper" && computerSelection == "rock" ) {
+        console.log("You won this round!") 
+        return 1; 
+    }
+    else if ( playerSelection == "scissors" && computerSelection == "paper" ) {
+        console.log("You won this round!")
+        return 1; 
+    }
+    else {
+        console.log("The computer wins this round") 
+        return 0;
+    }
+}
 
-getPlayerChoice function
-    create a playerChoice variable
-    ask the user to enter Rock, Paper or Scissors
-    convert choice to lowercase
-    validate if its correct
-    if correct store in playerSelection
+// Game starts running below:
 
-*/
+// playerSelection variable is hardcoded for now, but later it will be inputted by the user
 
+let playerSelection = "Paper";
 
+// The computerSelection is assigned running the getComputerChoice function
 
-/* 
+const computerSelection = getComputerChoice();
 
-playRound function with playerSelection and computerSelection as parameters
-    if playerSelection == computerSelection
-    return TIE
-    else if playerSelection == "rock" && computerSelection == "scissors"
-        print You won this round!
-        return 1
-    else if playerSelection == "paper" && computerSelection == "rock"
-        print You won this round!
-        return 1
-    else if playerSelection == "scissors" && computerSelection == "paper"
-        print You won this round!
-        return 1
-    else
-        print Computer wins this round!
+// The round is played taking the playerSelection and computerSelection into the playRound function. The result is printed on the console.
 
-*/
-
-/* 
-
-game function
-playground 5 times     
-
-*/
+console.log(playRound(playerSelection, computerSelection));
