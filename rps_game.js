b8705f1c4125a04103e465a99a5a719583154a88
martin-242 <martin242@gmail.com>
2023-11-08 11:51:45 +0100

    Initial commit

--- rps_game.js
+++ rps_game.js
@@ -1,3 +1,54 @@
+// Initializes game variables
+
+let playerScore = 0;
+let computerScore = 0;
+let roundNumber = 1;
+let computerSelection = 0;
+let playerSelection = 0;
+
+// DOM elements setup
+
+// Creates Round Info text
+const roundContainer = document.querySelector("#round_container");
+const roundContent = document.createElement("p");
+roundContent.textContent = "Round 1";
+roundContainer.appendChild(roundContent);
+
+// Creates Result Info text
+const resultContainer = document.querySelector("#results_container");
+const resultContent = document.createElement("p");
+resultContent.textContent = "Choose your move to start the game!";
+resultContainer.appendChild(resultContent);
+
+// Creates Score texts
+const playerScoreContainer = document.querySelector("#player_score");
+const playerScoreContent = document.createElement("p");
+playerScoreContent.textContent = `Player : ${playerScore}`;
+playerScoreContainer.appendChild(playerScoreContent);
+
+const computerScoreContainer = document.querySelector("#computer_score");
+const computerScoreContent = document.createElement("p");
+computerScoreContent.textContent = `${computerScore} : Computer`;
+computerScoreContainer.appendChild(computerScoreContent);
+
+// Creates "Start Game" button
+const startGameButtonContainer = document.querySelector("#star_game_button_container");
+const startGameButton = document.createElement("button");
+startGameButton.textContent = "Play Again";
+startGameButton.classList.add('restart_button');
+startGameButton.addEventListener('click', () => {
+    playerScore = 0;
+    computerScore = 0;
+    roundNumber = 1;
+    computerSelection = 0;
+    playerSelection = 0;
+    roundContent.textContent = `Round ${roundNumber}`;
+    playerScoreContent.textContent = `Player : ${playerScore}`;
+    computerScoreContent.textContent = `${computerScore} : Computer`;
+    resultContent.textContent = "Choose your move to start the game!";
+    startGameButtonContainer.removeChild(startGameButton);
+  });
+
 // getComputerChoice function:
 //      Selects a random number from 1 to 3 and stores it into a randomNumber variable
 //      Assigns to a variable computerSelection rock, paper or scissors depending on the number generated and returns it 
@@ -16,126 +67,90 @@ function getComputerChoice () {
         let computerSelection = "scissors";
         return computerSelection;
     }
-}
-
-// function getPlayerChoice:
-//      Asks the user to input choice and validates it, then returns the choice.
-
-function getPlayerChoice() {
-    let keepTrying = true;
-    while ( keepTrying == true ){
-        let playerInput = prompt("Please type Rock, Paper or Scissors:");
-        playerInput = playerInput.toLowerCase();
-        if ( (playerInput !== "rock") && (playerInput !== "paper") && (playerInput !== "scissors") ){
-            console.log("Invalid choice. Please type Rock, Paper or Scissors.");
-        }
-        else {
-            playerSelection = playerInput;
-            keepTrying = false;
-        }
-    }
-    return playerSelection;
-}
+};
 
 // playRound function:
 //      Takes the playerSelection and the computerSelection variables and compares them to determine who wins
 //      If player wins it returns 1 point and none if the computer wins.
 
 function playRound(playerSelection, computerSelection) {   
-        console.log(`Player selected: "${playerSelection}"\nComputer selected: "${computerSelection}"\n`);
-        if ( playerSelection == computerSelection ) {
-            console.log(`It's a tie!`);  
-            console.log(`Scores are: You "${playerScore}" - Computer "${computerScore}"`);
-            roundNumber = roundNumber + 1;
+    if (
+        ( playerSelection === "rock" && computerSelection === "scissors" ) ||
+        ( playerSelection === "paper" && computerSelection === "rock" ) || 
+        ( playerSelection === "scissors" && computerSelection === "paper" ) && 
+        ( computerScore <= 5 || playerScore <= 5 )) {
+        playerScore = playerScore + 1;
+        resultContent.textContent = (`Player selected: "${playerSelection}". Computer selected: "${computerSelection}". - You won this round!`);
+        roundNumber = roundNumber + 1;
+        roundContent.textContent = `Round ${roundNumber}`;
+        playerScoreContent.textContent = `Player : ${playerScore}`;
+        computerScoreContent.textContent = `${computerScore} : Computer`;
         }
-        else if ( playerSelection == "rock" && computerSelection == "scissors" ) {
-            playerScore = playerScore + 1;
-            console.log(`You won this round!`);
-            console.log(`Scores are: You "${playerScore}" - Computer "${computerScore}"`);
-            roundNumber = roundNumber + 1;
-            return playerScore;
+    else if (
+        ( playerSelection === "scissors" && computerSelection === "rock" ) ||
+        ( playerSelection === "rock" && computerSelection === "paper" ) || 
+        ( playerSelection === "paper" && computerSelection === "scissors" ) && 
+        ( computerScore <= 5 || playerScore <= 5 ))  {
+        computerScore = computerScore + 1;
+        resultContent.textContent = (`Player selected: "${playerSelection}". Computer selected: "${computerSelection}". - Computer won this round!`);
+        roundNumber = roundNumber + 1;
+        roundContent.textContent = `Round ${roundNumber}`;
+        playerScoreContent.textContent = `Player : ${playerScore}`;
+        computerScoreContent.textContent = `${computerScore} : Computer`;
         }
-        else if ( playerSelection == "paper" && computerSelection == "rock" ) {
-            playerScore = playerScore + 1;
-            console.log(`You won this round!`);
-            console.log(`Scores are: You "${playerScore}" - Computer "${computerScore}"`);
-            roundNumber = roundNumber + 1;
-            return playerScore;
+    else if ( playerSelection === computerSelection ) {
+        resultContent.textContent = (`Player selected: "${playerSelection}". Computer selected: "${computerSelection}". - It's a tie!`);
+        roundNumber = roundNumber + 1;
+        roundContent.textContent = `Round ${roundNumber}`;
         }
-        else if ( playerSelection == "scissors" && computerSelection == "paper" ) {
-            playerScore = playerScore + 1;
-            console.log(`You won this round!`);
-            console.log(`Scores are: You "${playerScore}" - Computer "${computerScore}"`);
-            roundNumber = roundNumber + 1;
-            return playerScore;
-        }
-        else {
-            computerScore = computerScore + 1;
-            console.log(`Computer won this round!`); 
-            console.log(`Scores are: You "${playerScore}" - Computer "${computerScore}"`);
-            roundNumber = roundNumber + 1;
-            return computerScore;
-        }
-}
-
-// Game function:
-//     Runs a for loop 5 times and on each round gets the user and computer choices and executes the playRound function. 
-//     If the player or the computer gets 3 points it wins, if not, whoever has more points in the 5th round wins.
-//     If the player and computer have the same points on round 5 then it's a tie.
-
-function game() {  
-    let keepGoing = true;  
-    console.log("Welcome to Rock/Paper/Scissors!");
-    for ( let i = 0; i <= 4; i++ ) {
-        if ( roundNumber == 1 ) {
-            console.log("Let's start! Round 1!");
-            }
-        else {
-            console.log(`Round ${roundNumber}`);  
-        }
-        console.log("Please make your selection!");
-        computerSelection = getComputerChoice();
-        playerSelection = getPlayerChoice();
-
-        console.log(playRound(playerSelection, computerSelection));
-        if ( playerScore == 3 ) {
-            console.log("You have won the game!");
-            return keepGoing = false;
-        }
-        else if ( computerScore == 3 ) {
-            console.log("The computer won the game!");
-            return keepGoing = false;
-        }
-        else if ( i == 4) {
-            console.log(`Game over! Final scores are:`);
-            console.log(`Player: ${playerScore} - Computer: ${computerScore}`);
-            if ( playerScore == computerScore ) {
-                console.log(`The game is a tie!`);
-                return keepGoing = false;
-            }
-            else if ( playerScore > computerScore ) {
-                console.log(`You won the game!`);
-                return keepGoing = false;
-            }
-            else {
-                console.log(`The computer won the game!`);
-                return keepGoing = false;
-            }
-        }
-        else {
-            console.log("Lets keep playing! Next round!");
-        }
+    if ( playerScore == 5 ) {
+        playerScoreContent.textContent = `Player : ${playerScore}`;
+        computerScoreContent.textContent = `${computerScore} : Computer`;
+        roundContent.textContent = `Round ${roundNumber}`;
+        startGameButtonContainer.appendChild(startGameButton);
     }
-}
+    else if ( computerScore == 5 ) {
+        playerScoreContent.textContent = `Player : ${playerScore}`;
+        computerScoreContent.textContent = `${computerScore} : Computer`;
+        roundContent.textContent = `Round ${roundNumber}`;
+        startGameButtonContainer.appendChild(startGameButton);
+    }
+};
 
-// Initializes game variables
+// Assigns events to buttons
 
-let playerScore = 0;
-let computerScore = 0;
-let roundNumber = 1;
-let computerSelection = 0;
-let playerSelection = 0;
+function addButtonValueRock(){
+    playerSelection = "rock";
+    computerSelection = getComputerChoice();
+    playRound(playerSelection, computerSelection);
+};
 
-// Game function starts the game
+function addButtonValuePaper(){
+    playerSelection = "paper";
+    computerSelection = getComputerChoice();
+    playRound(playerSelection, computerSelection);
+};
 
-game();
+function addButtonValueScissors(){
+    playerSelection = "scissors";
+    computerSelection = getComputerChoice();
+    playRound(playerSelection, computerSelection);
+};
+
+const btnRock = document.querySelector("#btn_rock");
+btnRock.addEventListener('click', addButtonValueRock);
+
+const btnPaper = document.querySelector("#btn_paper");
+btnPaper.addEventListener('click', addButtonValuePaper); 
+
+const btnScissors = document.querySelector("#btn_scissors");
+btnScissors.addEventListener('click', addButtonValueScissors);
+
+// Removes events once the score is met (not working)
+
+if ( playerScore === 5 || computerScore === 5 ) {
+    console.log("Its 5!");
+    btnRock.removeEventListener('click', addButtonValueRock);
+    btnPaper.removeEventListener('click', addButtonValuePaper);
+    btnScissors.removeEventListener('click', addButtonValueScissors);
+};
